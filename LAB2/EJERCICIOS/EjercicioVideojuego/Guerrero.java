a2595ed4497aaa0e130268e128a1595484e5d059
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-08-26 20:05:55 -0500

    Update Guerrero.java

--- LAB2/EJERCICIOS/EjercicioVideojuego/Guerrero.java
+++ LAB2/EJERCICIOS/EjercicioVideojuego/Guerrero.java
@@ -1,1 +1,17 @@
-a
+public class Guerrero extends PersonajeBase {
+    public Guerrero(String nombre) { super(nombre); }
+    
+    @Override
+    public void atacar(PersonajeBase enemigo) {
+    	if (!puedeAtacar(enemigo)) return;
+        System.out.println(getNombre() + " ataca con espada a " + enemigo.getNombre());
+        enemigo.recibirDanio(15);
+    }
+
+    @Override
+    public void usarHabilidad(PersonajeBase objetivo) {
+    	if (!puedeAtacar(objetivo)) return;
+        System.out.println(getNombre() + " usa Golpe Poderoso sobre " + objetivo.getNombre());
+        objetivo.recibirDanio(25);
+    }
+}
