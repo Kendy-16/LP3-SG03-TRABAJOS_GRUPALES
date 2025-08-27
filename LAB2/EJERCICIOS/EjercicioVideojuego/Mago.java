48896f1725a847ff13f37c1542f1411ae99a7e02
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-08-26 20:06:12 -0500

    Update Mago.java

--- LAB2/EJERCICIOS/EjercicioVideojuego/Mago.java
+++ LAB2/EJERCICIOS/EjercicioVideojuego/Mago.java
@@ -1,1 +1,17 @@
+public class Mago extends PersonajeBase {
+    public Mago(String nombre) { super(nombre); }
 
+    @Override
+    public void atacar(PersonajeBase enemigo) {
+    	if (!puedeAtacar(enemigo)) return;
+        System.out.println(getNombre() + " lanza rayo mágico a " + enemigo.getNombre());
+        enemigo.recibirDanio(10);
+    }
+
+    @Override
+    public void usarHabilidad(PersonajeBase objetivo) {
+    	if (!puedeAtacar(objetivo)) return;
+        System.out.println(getNombre() + " usa Bola de Fuego sobre " + objetivo.getNombre());
+        objetivo.recibirDanio(30);
+    }
+}
