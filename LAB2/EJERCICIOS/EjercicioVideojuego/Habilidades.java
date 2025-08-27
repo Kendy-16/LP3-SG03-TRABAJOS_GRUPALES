03149767cded92036ca79193519ac358238fef70
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-08-26 20:12:15 -0500

    Create Habilidades.java

---
+++ LAB2/EJERCICIOS/EjercicioVideojuego/Habilidades.java
@@ -0,0 +1,12 @@
+public class Habilidad {
+    private String nombre;
+    private int poder;
+
+    public Habilidad(String nombre, int poder) {
+        this.nombre = nombre;
+        this.poder = poder;
+    }
+
+    public String getNombre() { return nombre; }
+    public int getPoder() { return poder; }
+}
