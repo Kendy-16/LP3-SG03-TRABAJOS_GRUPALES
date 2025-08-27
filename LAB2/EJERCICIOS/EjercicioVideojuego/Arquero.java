ca7e43662036724df13e83514ee45c3f8ec6d138
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-08-26 20:06:36 -0500

    Update Arquero.java

--- LAB2/EJERCICIOS/EjercicioVideojuego/Arquero.java
+++ LAB2/EJERCICIOS/EjercicioVideojuego/Arquero.java
@@ -1,1 +1,19 @@
+package GestionDeVideojuego;
 
+public class Arquero extends PersonajeBase {
+    public Arquero(String nombre) { super(nombre); }
+
+    @Override
+    public void atacar(PersonajeBase enemigo) {
+    	if (!puedeAtacar(enemigo)) return;
+        System.out.println(getNombre() + " dispara una flecha a " + enemigo.getNombre());
+        enemigo.recibirDanio(12);
+    }
+
+    @Override
+    public void usarHabilidad(PersonajeBase objetivo) {
+    	if (!puedeAtacar(objetivo)) return;
+        System.out.println(getNombre() + " usa Flecha Explosiva sobre " + objetivo.getNombre());
+        objetivo.recibirDanio(20);
+    }
+}
