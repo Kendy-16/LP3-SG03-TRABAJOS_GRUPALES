6603549ed402633ed28c74071f0145411df34e3f
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-08-27 22:35:07 -0500

    Update ContadorTest.java

--- LAB2/ACTIVIDADES/ActividadN_2/ContadorTest.java
+++ LAB2/ACTIVIDADES/ActividadN_2/ContadorTest.java
@@ -1,17 +1,34 @@
 public class ContadorTest {
     public static void main(String[] args) {
-        Contador c1, c2;
-        System.out.println(Contador.acumulador());
+        System.out.println("Acumulador inicial: " + Contador.getAcumulador());
+        System.out.println("nContadores inicial: " + Contador.getNContadores());
+        System.out.println("ultimoContador inicial: " + Contador.getUltimoContador());
+        System.out.println();
 
-        c1 = new Contador(4);
-        c2 = new Contador(16);
-        
-        c1.inc();
-        c1.inc();
-        c2.inc();
+        Contador cDefault = new Contador(); // usa VALOR_INICIAL = 10
+        System.out.println("Tras crear cDefault:");
+        System.out.println("  cDefault.valor = " + cDefault.getValor());
+        System.out.println("  acumulador = " + Contador.getAcumulador());
+        System.out.println("  nContadores = " + Contador.getNContadores());
+        System.out.println("  ultimoContador = " + Contador.getUltimoContador());
+        System.out.println();
 
-        System.out.println("Valor c1: " + c1.getValor());
-        System.out.println("Valor c2: " + c2.getValor());
-        System.out.println("Acumulador: " + Contador.getAcumulador());
+        Contador cCustom = new Contador(5);
+        System.out.println("Tras crear cCustom(5):");
+        System.out.println("  cCustom.valor = " + cCustom.getValor());
+        System.out.println("  acumulador = " + Contador.getAcumulador());
+        System.out.println("  nContadores = " + Contador.getNContadores());
+        System.out.println("  ultimoContador = " + Contador.getUltimoContador());
+        System.out.println();
+
+        // probar inc()
+        cDefault.inc();
+        cCustom.inc();
+        System.out.println("Después de cDefault.inc() y cCustom.inc():");
+        System.out.println("  cDefault.valor = " + cDefault.getValor());
+        System.out.println("  cCustom.valor = " + cCustom.getValor());
+        System.out.println("  acumulador = " + Contador.getAcumulador());
+        System.out.println("  nContadores = " + Contador.getNContadores());
+        System.out.println("  ultimoContador = " + Contador.getUltimoContador());
     }
 }
