052d3f8d2172a78f63c267c61457eb0b688d90b5
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-08-27 19:20:28 -0500

    Create Persona.java

---
+++ LAB2/ACTIVIDADES/ActividadN_4/Persona.java
@@ -0,0 +1,52 @@
+public class Persona {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private Cuenta cuenta; // relación de composición
+
+    // Constructor
+    public Persona(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cuenta = new Cuenta(id); // composición: Persona crea su propia Cuenta
+    }
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona [id=" + id + ", nombre=" + nombre + " " + apellido + ", " + cuenta.toString() + "]";
+    }
+}
