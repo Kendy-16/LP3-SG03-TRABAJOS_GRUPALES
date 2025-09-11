c4250cb3a7c2d0b9ec4f1f7a65c41ab7dd777a4a
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-09-11 10:35:37 -0500

    Update RegistroEstudiante.java

--- LAB4/EJERCICIOS/EjercicioN_4/RegistroEstudiante.java
+++ LAB4/EJERCICIOS/EjercicioN_4/RegistroEstudiante.java
@@ -3,35 +3,35 @@ import java.util.NoSuchElementException;
 
 //Excepcion Personalizada
 
-class Excepcion_de_blanco extends Exception{
-    public Excepcion_de_blanco() {
-        super("Excepción de blanco: Espacio en blanco detectado");
-    }
+class Excepcion_de_blanco extends Exception {
+	public Excepcion_de_blanco() {
+		super("(!) Excepcion de blanco: Espacio en blanco detectado");
+	}
 }
 
 //Clase Estudiante
 class Estudiante {
-    private String nombre;
-    
-    public Estudiante(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	private String nombre;
+
+	public Estudiante(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
 
 //Clase Registro de estudiantes
 class RegistroEstudiante {
-    private ArrayList <Estudiante> estudiantes;
-    
-    public RegistroEstudiante() {
-        ArrayList <Estudiante> estudiantes = new ArrayList<>();
-    }
-    
+	private ArrayList <Estudiante> estudiantes;
+
+	public RegistroEstudiante() {
+		estudiantes = new ArrayList<>();
+	}
+
 	public ArrayList <Estudiante> getEstudiantes() {
 		return estudiantes;
 	}
@@ -39,40 +39,66 @@ class RegistroEstudiante {
 	public void setEstudiantes(ArrayList <Estudiante> estudiantes) {
 		this.estudiantes = estudiantes;
 	}
-	
+
 	public void agregarEstudiante(Estudiante nuevo) throws Excepcion_de_blanco {
-        for (int i = 0; i < nuevo.getNombre().length(); i ++) {
-            if(nuevo.getNombre().charAt(i) ==  ' ') {
-                throw new Excepcion_de_blanco();
+		for (int i = 0; i < nuevo.getNombre().length(); i ++) {
+			if(nuevo.getNombre().charAt(i) ==  ' ') {
+				throw new Excepcion_de_blanco();
+			}
+		}
+		estudiantes.add(nuevo);
+		System.out.println("Se agrego a estudiante " + nuevo.getNombre() +" con exito");
+	}
+
+	public void buscarEstudiante(Estudiante estudiante) {
+        
+        boolean encontrado = false;
+    
+			for(Estudiante e : estudiantes) {
+				if(e.getNombre().equals(estudiante.getNombre())) {
+				    encontrado = true;
+					System.out.println("El estudiante: " +  estudiante.getNombre() + " fue encontrado");
+					break;
+				}
+			}
+			
+            if (!encontrado) {
+                throw new NoSuchElementException("\n(!) Estudiante " + estudiante.getNombre() + " NO fue encontrado");
             }
-        }
-        System.out.println("Se agrego a estudiante con exito");
 	}
 	
-	public void buscarEstudiante(Estudiante estudiante) {
-	    
-	    boolean encontrado = false;
-	    try {
-	    
-    	    for(Estudiante e : estudiantes) {
-    	        if(e == estudiante) {
-    	            encontrado = true;
-    	            System.out.println("El estudiante: " +  estudiante.getNombre() + "fue encontrado");
-    	        } 
-    	    }
-	    } catch (NoSuchElementException o) {
-	       System.out.println("(!) El estudiante: " +  estudiante.getNombre() + "no fue encontrado");
-            throw new NoSuchElementException ("Error: El valor no puede ser negativo. Valor ingresado: " + estudiante.getNombre());
-	    }
-	}
 }
 
 class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Estudiante estudiante1 = new Estudiante("Jose");
         Estudiante estudiante2 = new Estudiante("Laura");
-        
-        RegistroEstudiante registro = new RegistroEstudiante();
-        registro.agregarEstudiante(estudiante1);
-    }
+        Estudiante estudiante3 = new Estudiante(" "); 
+        Estudiante estudiante4 = new Estudiante("Fernando");
+
+
+		RegistroEstudiante registro = new RegistroEstudiante();
+
+		try {
+            registro.agregarEstudiante(estudiante1);  
+            registro.agregarEstudiante(estudiante2);  
+            registro.agregarEstudiante(estudiante3);  
+
+
+		} catch (Excepcion_de_blanco e) {
+
+			System.out.println("Error al agregar a estudiante\n" + e.getMessage());
+
+		}
+		
+        try {
+            
+            registro.buscarEstudiante(estudiante3);  
+            
+        } catch (NoSuchElementException e) {
+            
+            System.out.println("Error al buscar estudiante "+ e.getMessage());
+            
+        }
+	}
 }
