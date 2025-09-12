d6b00702af0acb1cab65bc53c2a2c6f42493ff09
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2025-09-11 22:15:56 -0500

    Create ExcepcionFormatoCorreo.java

---
+++ LAB4/EJERCICIOS/EjercicioN_5/ExcepcionFormatoCorreo.java
@@ -0,0 +1,5 @@
+class ExcepcionFormatoCorreo extends Exception {
+    public ExcepcionFormatoCorreo() {
+        super("(!) Excepción de formato: El correo electrónico no tiene un formato válido");
+    }
+}
