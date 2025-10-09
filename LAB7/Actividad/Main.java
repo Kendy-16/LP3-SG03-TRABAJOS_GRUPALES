4a4e6c538f03406c5d6b24136541dc53a43d3d38
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-10-09 10:55:23 -0500

    Update Main.java

--- LAB7/Actividad/Main.java
+++ LAB7/Actividad/Main.java
@@ -6,20 +6,22 @@ public class Main {
         gestor.mostrar();
 
         System.out.println("\n== Agregando personajes ==");
-        gestor.agregar(new Personaje("Alvaro", 120, 25, 15, 3));
-        gestor.agregar(new Personaje("Bea", 90, 30, 10, 2));
+        gestor.agregar(new Personaje("Konnor", 120, 25, 15, 3));
+        gestor.agregar(new Personaje("Retep", 100, 30, 5, 4));
+        gestor.agregar(new Personaje("Paithen", 90, 30, 10, 2));
+        gestor.agregar(new Personaje("Lydiac", 110, 28, 20, 4));
 
         System.out.println("\n== Lista después de agregar ==");
         gestor.mostrar();
 
-        System.out.println("\n== Modificando a Bea ==");
-        gestor.modificar("Bea", 100, 40, 20, 3);
+        System.out.println("\n== Modificando a Konnor ==");
+        gestor.modificar("Konnor", 100, 40, 20, 8);
 
         System.out.println("\n== Lista después de modificar ==");
         gestor.mostrar();
 
-        System.out.println("\n== Borrando a Alvaro ==");
-        gestor.borrar("Alvaro");
+        System.out.println("\n== Borrando a Paithen ==");
+        gestor.borrar("Paithen");
 
         System.out.println("\n== Lista final ==");
         gestor.mostrar();
