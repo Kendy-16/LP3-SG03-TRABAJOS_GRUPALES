fb943530a7a6b73cb770a56a1f2912db1ee184a7
Kendy-16 <kelly.ramos.1652@gmail.com>
2025-10-09 10:53:08 -0500

    Update Main.java

--- LAB7/Actividad/Main.java
+++ LAB7/Actividad/Main.java
@@ -1,1 +1,27 @@
-//pal main
+public class Main {
+    public static void main(String[] args) {
+        Gestor gestor = new Gestor();
+
+        System.out.println("== Lista inicial ==");
+        gestor.mostrar();
+
+        System.out.println("\n== Agregando personajes ==");
+        gestor.agregar(new Personaje("Alvaro", 120, 25, 15, 3));
+        gestor.agregar(new Personaje("Bea", 90, 30, 10, 2));
+
+        System.out.println("\n== Lista después de agregar ==");
+        gestor.mostrar();
+
+        System.out.println("\n== Modificando a Bea ==");
+        gestor.modificar("Bea", 100, 40, 20, 3);
+
+        System.out.println("\n== Lista después de modificar ==");
+        gestor.mostrar();
+
+        System.out.println("\n== Borrando a Alvaro ==");
+        gestor.borrar("Alvaro");
+
+        System.out.println("\n== Lista final ==");
+        gestor.mostrar();
+    }
+}
